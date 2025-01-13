d336c85827206dbeb308eddbaecba70693f881c3
DavidHe233 <bywise233@outlook.com>
2025-01-13 09:02:40 +0800

    format

--- app/src/test/java/vlookup/AppTest.java
+++ app/src/test/java/vlookup/AppTest.java
@@ -3,42 +3,34 @@
  */
 package vlookup;
 
-import javax.swing.JFrame;
-
-import com.fazecast.jSerialComm.SerialPort;
-
-import vlookup.display.Display;
-import vlookup.utils.IOUtil;
-import vlookup.utils.StringUtil;
-import javax.swing.*;
-import java.awt.*;
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
+import javax.swing.JFrame;
+
+import vlookup.display.Display;
+
 class AppTest {
   public static void main(String[] args) {
-    try{
-      FileReader file=new FileReader("C:\\Users\\Zijie-Zhang\\OneDrive\\文档\\小马过河.txt");
+    try {
+      FileReader file = new FileReader("C:\\Users\\Zijie-Zhang\\OneDrive\\文档\\小马过河.txt");
       BufferedReader f = new BufferedReader(file);
-      String line=f.readLine();
+      String line = f.readLine();
       System.out.println(line);
       JFrame frame = new JFrame("Draw Points");
       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                while (line!=null){
-                  System.out.println(line);
-                  Display.ddd(line, frame);
-                  line=f.readLine();
-                  try {
-                    Thread.sleep(1000);
-                }
-                catch (Exception e){
-                }
-                }
-    }
-    catch(IOException e){
-      System.out .println("file not found");
+      while (line != null) {
+        System.out.println(line);
+        Display.ddd(line, frame);
+        line = f.readLine();
+        try {
+          Thread.sleep(1000);
+        } catch (Exception e) {
+        }
+      }
+    } catch (IOException e) {
+      System.out.println("file not found");
     }
     /*SerialPort port=IOUtil.setPort();
         JFrame frame = new JFrame("Draw Points");
@@ -54,5 +46,6 @@ class AppTest {
         */
 
   }
-  
+
 }
+
