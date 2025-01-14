bdb9b6dbcdf119da1bbae84930ecc107b716d8a7
DavidHe233 <bywise233@outlook.com>
2025-01-14 21:41:46 +0800

    update gesture & mathUtil

--- app/src/main/java/vlookup/utils/Gesture.java
+++ app/src/main/java/vlookup/utils/Gesture.java
@@ -1,5 +1,15 @@
 package vlookup.utils;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Gesture {
-    
+    public static boolean isSameLevel(List<Coordinate> coordinates) {
+        List<Integer> x = new ArrayList<>();
+        for (Coordinate c : coordinates) {
+            x.add(c.y());
+        }
+        double sd = MathUtil.standardDeviation(x);
+        return sd < 10;
+    }
 }
